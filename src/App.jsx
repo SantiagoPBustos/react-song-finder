96b04fb005cf2606d87a79eda3c2292fb1fc7721
SantiagoPBustos <david.pina@uptc.edu.co>
2023-03-06 17:23:14 -0500

    config project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import SongFInder from '../components/SongFinder'
 import './App.css'
 
 function App() {
   return (
     <>
-    
+      <h3>Buscador de Canciones</h3>
+      <SongFInder/>
     </>
   )
 }
