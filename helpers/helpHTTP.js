e0acd3f8d37551d7ca0b37e5b384b75649b8f32e
SantiagoPBustos <david.pina@uptc.edu.co>
2023-03-10 12:18:08 -0500

    update form}

---
+++ helpers/helpHTTP.js
@@ -0,0 +1,56 @@
+export const HelperHTTP = () => {
+    const customFetch = (endpoint, options) => {
+        const defaultHeader = {
+            accept: "application/json",
+        };
+
+        const controller = new AbortController();
+        options.signal = controller.signal;
+
+        options.method = options.method || "GET";
+        options.headers = options.headers ? {
+            ...defaultHeader,
+            ...options.headers
+        } : defaultHeader;
+
+        options.body = JSON.stringify(options.body) || false;
+        if (!options.body) delete options.body
+
+        setTimeout(() => { controller.abort() }, 3000);
+
+        return fetch(endpoint, options)
+            .then((response) => response.ok ?
+                response.json()
+                : Promise.reject(
+                    {
+                        error: true,
+                        status: response.status || "00",
+                        statusText: response.statusText || "Ups!, ¡Ocurrio un error inesperado!",
+                    }
+                ))
+            .catch((error) => error);
+
+    }
+
+    const get = (url, options = {}) => customFetch(url, options);
+
+    const post = (url, options = {}) => {
+        options.method = "POST";
+        return customFetch(url, options);
+    };
+    const put = (url, options = {}) => {
+        options.method = "PUT";
+        return customFetch(url, options);
+    }
+    const del = (url, options = {}) => {
+        options.method = "DELETE";
+        return customFetch(url, options);
+    }
+
+    return {
+        get,
+        post,
+        put,
+        del
+    };
+}
