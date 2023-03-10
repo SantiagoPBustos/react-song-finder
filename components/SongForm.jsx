9f484c1f212c0dcc57e4a9f190cab7a2e49f5633
SantiagoPBustos <david.pina@uptc.edu.co>
2023-03-10 12:09:33 -0500

    update form

--- components/SongForm.jsx
+++ components/SongForm.jsx
@@ -9,12 +9,26 @@ function SongForm(handleSearch) {
     const [form, setForm] = useState(initForm);
 
     const handleChange = (e) => {
+        setForm({...initForm, 
+        [e.target.name]: e.target.value,
+    });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!form.artist || !form.song) {
+            alert("Datos Incompletos!");
+            return;
+        }
+
+        handleSearch(form);
+        setForm(initForm);
 
     }
-
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" name='artist' placeholder='Nombre artista' onChange={handleChange} value={form.artist} />
                 <input type="text" name='song' placeholder='Nombre de canción' onChange={handleChange} value={form.song} />
                 <input type="submit" value='Buscar'/>
